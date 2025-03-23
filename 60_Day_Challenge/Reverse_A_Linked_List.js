19b7492adf04d58e97cc28cc22c63ebd1687b8f8
Vijayarahavan <vijayarahavan176@gmail.com>
2025-03-23 16:14:50 +0530

    feat: Day 7 of 60 day challenge

---
+++ 60_Day_Challenge/Reverse_A_Linked_List.js
@@ -0,0 +1,33 @@
+class ListNode {
+  val = 0;
+  next = null;
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+const createList = (arr) => {
+  let obj = null;
+  for (let i of arr) {
+    obj = new ListNode(i, obj);
+  }
+
+  return obj;
+};
+
+const list = createList([1, 2, 3, 4, 5].reverse());
+
+function reverseList(head) {
+  let obj = new ListNode(head.val),
+    cur = head.next;
+
+  while (cur !== null) {
+    const v = new ListNode(cur.val, obj);
+    obj = v;
+    cur = cur.next;
+  }
+  console.log(obj);
+}
+
+reverseList(list);
